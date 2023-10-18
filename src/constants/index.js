0814e1df0a9db5d9ec5ac7e342a94a2395c51c11
Russellkusuma <russellkusuma11@gmail.com>
2023-10-17 18:09:23 -0700

    lavalab demo

---
+++ src/constants/index.js
@@ -0,0 +1,109 @@
+import {facebook, instagram, linkedin, twitter} from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.google.com/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.google.com/",
+      },
+      {
+        name: "Create",
+        link: "https://www.google.com/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.google.com/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.google.com/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.google.com/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.google.com/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.google.com/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.google.com/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.google.com/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.google.com/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.google.com/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
